6518a2f60fb56f1b25439d7601d81381605fbd5a
Harsh Thorat <harshthorat18@gmail.com>
2025-05-23 16:50:54 +0530

    Update server

--- client/server.js
+++ client/server.js
@@ -1,52 +1,45 @@
 const express = require("express");
 const colors = require("colors");
-const moragan = require("morgan");
+const morgan = require("morgan"); // fix typo here
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const path = require("path");
 
-const path = require("path")
-
-//dotenv conig
+// dotenv config
 dotenv.config();
 
-
-//mongodb connection
+// mongodb connection
 connectDB();
 
-
-//rest obejct
+// rest object
 const app = express();
 
-//middlewares
+// middlewares
 app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
-app.use(moragan("dev"));
+app.use(morgan("dev")); // fixed typo
 
-//routes
+// routes
 app.use("/api/v1/user", require("./routes/userRout"));
-app.use('/api/v1/admin',require('./routes/adminRouters'))
-app.use('/api/v1/doctor',require('./routes/doctorRouters'))
+app.use("/api/v1/admin", require("./routes/adminRouters"));
+app.use("/api/v1/doctor", require("./routes/doctorRouters"));
 
-app.use(express.json());
+// Serve static files from the React app build folder
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-//STATIC FILE
-app.use(express.static(path.join(__dirname, './client/build')))
+// The "catchall" handler for SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
-app.get('*' , function(req,res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'))
-})
-
-//port
+// port
 const port = process.env.PORT || 8080;
-//listen port
+
+// listen port
 app.listen(port, () => {
   console.log(
-    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${port}`
       .bgCyan.white
   );
 });
-
-
-
-
